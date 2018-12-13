2b15649ded3b8732150621d0645bd6f22b8c7727
bobysf12 <bobby_santoso@rocketmail.com>
2018-12-13 08:30:12 +0700

    Fix commentt bug

--- src/redux/comments/selectors.ts
+++ src/redux/comments/selectors.ts
@@ -1,15 +1,17 @@
 import { RootState } from "..";
-import { Comment } from "./types";
 
 const getCommentsState = (state: RootState) => state.comments;
 const getCommentsStateByEventId = (eventId: string) => (state: RootState) => getCommentsState(state)[eventId];
 
 export const getEventComments = (eventId: string) => (state: RootState) => {
 	const commentsState = getCommentsStateByEventId(eventId)(state);
-	if (!commentsState) {
+
+	if (!commentsState || !commentsState.commentsMap) {
 		return [];
 	}
-	const commentsMap: Record<string, Comment> = commentsState.commentsMap;
+
+	const { commentsMap } = commentsState;
+
 	return Object.keys(commentsMap)
 		.map(k => commentsMap[k])
 		.sort((a, b) => a.created_at.getTime() - b.created_at.getTime());
